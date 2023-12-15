244be707a6a9027d059698dcbc3bea177d3a6605
Thomas TORRES <114108420+Thomas22206512@users.noreply.github.com>
2023-12-16 00:29:49 +0100

    Update Main.java

--- New Java Introduction/src/lerenard/Main.java
+++ New Java Introduction/src/lerenard/Main.java
@@ -2,6 +2,41 @@ package lerenard;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        // Commentaire
+
+        /*
+        Commentaire sur
+        plusieur ligne
+         */
+
+        // Variable
+
+        /*
+        On défini en premier le datatype puis le
+        nom de la variable pour faire un déclaration
+
+        ou
+
+        On défini le datatype puis le nom de la variable
+        et  on lui attribue une valeur pour faire une initialisation
+
+        on fini toutes les instruction par ;
+         */
+
+        int points;
+        points = 100;
+
+        int x = -69;
+
+        float pi = 3.1415f;
+        double e = 2.718;
+
+        boolean active_input = true;
+        boolean active_box = false;
+
+        char grade = 'B';
+        String name = "LeRenard";
+
+        System.out.println(name);
     }
 }
